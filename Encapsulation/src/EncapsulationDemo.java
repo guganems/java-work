496b67d6cead11bdf006a0041801769d16d061f3
gnems <g_nemsveridze@cu.edu.ge>
2019-01-15 16:01:46 +0400

    Encapsulation

---
+++ Encapsulation/src/EncapsulationDemo.java
@@ -0,0 +1,34 @@
+// Encapsulation -> Binding data with methods
+class Student{
+    private int rollno;
+    private String name;
+
+    public int getRollno() {
+        System.out.println("User is accessing the value");
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+        System.out.println("Value of rollno changed");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setRollno(2);
+        s1.setName("Guga");
+
+        System.out.println(s1.getRollno());
+        System.out.println(s1.getName());
+    }
+}
