4178b2e405f4be14e17ce944143abe249ec538c0
gnems <g_nemsveridze@cu.edu.ge>
2019-01-15 14:34:54 +0400

    Abstract classes and methods

---
+++ Java/src/game/GameObject.java
@@ -0,0 +1,25 @@
+package game;
+
+public abstract class GameObject {
+
+    // this method must be implemented by any class that extends GameObject
+    public abstract void draw();
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.someMethod();
+//        player.draw();
+
+        Menu menu = new Menu();
+//        menu.draw();
+
+        GameObject[] gameObjects = new GameObject[2];
+        gameObjects[0] = player;
+        gameObjects[1] = menu;
+
+        for (GameObject obj : gameObjects){
+            obj.draw();
+        }
+        // abstract classes exist to be extended, they can not be instantiated
+    }
+}
